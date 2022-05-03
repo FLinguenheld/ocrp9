b6d3cd72e256592357874244893d5a2dcc39cd2d
FLinguenheld <f@linguenheld.fr>
2022-05-03 21:34:17 +0200

    Test review

--- ticket/urls.py
+++ ticket/urls.py
@@ -6,5 +6,6 @@ from ticket import views
 
 urlpatterns = [
     path('ticket/list/', views.list_tickets, name='ticket_list'),
-    path('ticket/add/', views.add_ticket, name='ticket_add')
+    path('ticket/add/', views.add_ticket, name='ticket_add'),
+    path('ticket/<int:id>/', views.ticket, name='ticket')
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -4,6 +4,12 @@ from ticket.models import Ticket
 from ticket.forms import TicketForm
 
 
+def ticket(request, id):
+    
+    t = Ticket.objects.get(id=id)
+    return render(request, 'ticket.html', {'ticket': t})
+
+
 def list_tickets(request):
 
     tickets = Ticket.objects.all()
@@ -23,3 +29,17 @@ def add_ticket(request):
         form = TicketForm()
 
     return render(request, 'add.html', {'form': TicketForm()})
+
+def update_ticket(request, id):
+
+    ticket = Ticket.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = TicketForm(request.POST, instance=ticket)
+
+        if form.is_valid():
+            form.save()
+            return redirect('ticket_list')
+
+    return render(request, 'update.html', {'form': form})
+   
