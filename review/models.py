10bd737ad14e0f4255fdd612c3e91c2bd264f2f1
FLinguenheld <f@linguenheld.fr>
2022-05-04 09:34:27 +0200

    Up ticket

--- review/models.py
+++ review/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from ticket.models import Ticket
+
+class Review(models.Model):
+
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    rating = models.fields.PositiveSmallIntegerField(max_length=1024, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.fields.CharField(max_length=128)
+    body = models.fields.TextField()
+    time_created = models.fields.DateTimeField(auto_now_add=True)
