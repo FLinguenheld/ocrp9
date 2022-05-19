34f48dece27c7cc7b32f08459408706317725ba0
FLinguenheld <f@linguenheld.fr>
2022-05-19 23:43:38 +0200

    Ticketing management ok

--- ticketing/forms.py
+++ ticketing/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-
+from django.forms import CharField, RadioSelect, ChoiceField
 from . import models
 
 
@@ -17,9 +17,13 @@ class TicketForm(ModelForm):
                                                         'placeholder': 'Description'})
 
 class ReviewForm(ModelForm):
+
+    RATING = [(str(x), x) for x in range(0,6)]
+    rating_choice = ChoiceField(widget=RadioSelect(attrs={'class': 'form-check-input ml-2'}), choices=RATING, label='Note :')
+
     class Meta():
         model = models.Review
-        exclude = ('user', 'ticket', 'time_created', 'time_edited')
+        fields = ('headline', 'rating_choice', 'body')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -28,3 +32,5 @@ class ReviewForm(ModelForm):
 
         self.fields['body'].widget.attrs.update({'class': 'w-100 text-justify',
                                                   'placeholder': 'Critique'})
+
+        # self.fields['rating_choice'].widget.attrs.update({'class': 'w-100 form-check-input'})
--- ticketing/views.py
+++ ticketing/views.py
@@ -22,12 +22,57 @@ class HomeView(LoginRequiredMixin, View):
             ticket = Ticket.objects.filter(id=review.ticket.id)
             review.ticket_ooject = ticket.first()
 
+            rating = review.rating
+            review.rating = ['Y' for y in range(rating)]
+            review.rating += ['N' for n in range(rating, 5)]  # How getting the max validator ?
+
         # lists all user's tikets
-        # tickets = Ticket.objects.filter(user=request.user)
         tickets = Ticket.objects.all()
+        for t in tickets:
+            if Review.objects.filter(ticket=t.id):
+                t.already_reviewed = True
+            else:
+                t.already_reviewed = False
 
         return render(request, self.template_name, context={'reviews': reviews,
                                                              'tickets': tickets})
+
+
+class CreateCompleteReviewView(LoginRequiredMixin, View):
+    template_name = 'ticketing/create_complete_review.html'
+    title = "Création d'un ticket et de sa critique"
+
+    def get(self, request):
+        form_ticket = forms.TicketForm()
+        form_review = forms.ReviewForm(initial={'rating_choice': 5})
+
+        return render(request, self.template_name, context={'form_ticket': form_ticket,
+                                                            'form_review': form_review,
+                                                            'title': self.title,
+                                                            'text_button': 'Créer et publier'})
+
+    def post(self, request):
+        form_ticket = forms.TicketForm(request.POST, request.FILES)
+        form_review = forms.ReviewForm(request.POST, initial={'rating_choice': 5})
+
+        if all([form_ticket.is_valid(), form_review.is_valid()]):
+            ticket = form_ticket.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+
+            review = form_review.save(commit=False)
+            review.ticket = ticket
+            review.user = request.user
+            review.rating = form_review.cleaned_data.get('rating_choice')
+            review.save()
+
+            return redirect('home')
+
+        else:
+            return render(request, self.template_name, context={'form_ticket': form_ticket,
+                                                                'form_review': form_review,
+                                                                'title': self.title,
+                                                                'text_button': 'Créer et publier'})
 
 
 class CreateTicketView(LoginRequiredMixin, View):
@@ -36,9 +81,9 @@ class CreateTicketView(LoginRequiredMixin, View):
 
     def get(self, request):
         form = forms.TicketForm()
-        return render(request, self.template_name, context={'form': form,
-                                                             'title': self.title,
-                                                             'text_button': 'Créer'})
+        return render(request, self.template_name, context={'form_ticket': form,
+                                                            'title': self.title,
+                                                            'text_button': 'Créer'})
 
     def post(self, request):
         form = forms.TicketForm(request.POST, request.FILES)
@@ -51,9 +96,9 @@ class CreateTicketView(LoginRequiredMixin, View):
             return redirect('home')
 
         else:
-            return render(request, self.template_name, context={'form': form,
-                                                                 'title': self.title,
-                                                                 'text_button': 'Créer'})
+            return render(request, self.template_name, context={'form_ticket': form,
+                                                                'title': self.title,
+                                                                'text_button': 'Créer'})
 
 
 class UpdateTicketView(LoginRequiredMixin, View):
@@ -63,9 +108,9 @@ class UpdateTicketView(LoginRequiredMixin, View):
     def get(self, request, ticket_id):
         ticket = Ticket.objects.get(id=ticket_id)
         form = forms.TicketForm(instance=ticket)
-        return render(request, self.template_name, context={'form': form,
-                                                             'title': self.title,
-                                                             'text_button': 'Modifier'})
+        return render(request, self.template_name, context={'form_ticket': form,
+                                                            'title': self.title,
+                                                            'text_button': 'Modifier'})
 
     def post(self, request, ticket_id):
         ticket = Ticket.objects.get(id=ticket_id)
@@ -79,7 +124,7 @@ class UpdateTicketView(LoginRequiredMixin, View):
             return redirect('home')
 
         else:
-            return render(request, self.template_name, context={'form': form,
+            return render(request, self.template_name, context={'form_ticket': form,
                                                                  'title': self.title,
                                                                  'text_button': 'Modifier'})
 
@@ -104,30 +149,31 @@ class CreateReviewView(LoginRequiredMixin, View):
 
     def get(self, request, ticket_id):
         ticket = Ticket.objects.get(id=ticket_id)
-        form = forms.ReviewForm()
+        form = forms.ReviewForm(initial={'rating_choice': 5})
 
         return render(request, self.template_name, context={'t': ticket,
-                                                             'form': form,
-                                                             'title': self.title,
-                                                             'text_button': 'Publier'})
+                                                            'form_review': form,
+                                                            'title': self.title,
+                                                            'text_button': 'Publier'})
 
     def post(self, request, ticket_id):
         ticket = Ticket.objects.get(id=ticket_id)
-        form = forms.ReviewForm(request.POST)
+        form = forms.ReviewForm(request.POST, initial={'rating_choice': 5})
 
         if form.is_valid():
             review = form.save(commit=False)
             review.ticket = ticket
             review.user = request.user
+            review.rating = form.cleaned_data.get('rating_choice')
             review.save()
 
             return redirect('home')
 
         else:
             return render(request, self.template_name, context={'t': ticket,
-                                                                 'form': form,
-                                                                 'title': self.title,
-                                                                 'text_button': 'Publier'})
+                                                                'form_review': form,
+                                                                'title': self.title,
+                                                                'text_button': 'Publier'})
 
 
 class UpdateReviewView(LoginRequiredMixin, View):
@@ -137,28 +183,29 @@ class UpdateReviewView(LoginRequiredMixin, View):
     def get(self, request, review_id):
         review = Review.objects.get(id=review_id)
         ticket = Ticket.objects.get(id=review.ticket.id)
-        form = forms.ReviewForm(instance=review)
+        form = forms.ReviewForm(instance=review, initial={'rating_choice': review.rating})
 
         return render(request, self.template_name, context={'t': ticket,
-                                                            'form': form,
+                                                            'form_review': form,
                                                             'title': self.title,
                                                             'text_button': 'Modifier'})
 
     def post(self, request, review_id):
         review = Review.objects.get(id=review_id)
         ticket = Ticket.objects.get(id=review.ticket.id)
-        form = forms.ReviewForm(request.POST, instance=review)
+        form = forms.ReviewForm(request.POST, instance=review, initial={'rating_choice': review.rating})
 
         if form.is_valid():
             review = form.save(commit=False)
             review.time_edited = datetime.today()
+            review.rating = form.cleaned_data.get('rating_choice')
             review.save()
 
             return redirect('home')
 
         else:
             return render(request, self.template_name, context={'t': ticket,
-                                                                'form': form,
+                                                                'form_review': form,
                                                                 'title': self.title,
                                                                 'text_button': 'Modifier'})
         
