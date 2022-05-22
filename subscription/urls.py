0730ddba6442055430b4a83f3a42f070f04f6ff8
FLinguenheld <f@linguenheld.fr>
2022-05-22 23:25:56 +0200

    Subscription logic ok

--- subscription/urls.py
+++ subscription/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     # path('home/', views.HomeView.as_view(), name='home'),
     path('subscription-management/', views.SubscriptionManagementView.as_view(), name='subscription_management'),
-    path('subscription/<int:userfollows_id>/delete/', views.DeleteSubscriptionView.as_view(), name='delete_subscription'),
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -5,40 +5,59 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . import forms
 from .models import UserFollows
+from authentication.models import User
 
+from django.utils.datastructures import MultiValueDict
 
 class SubscriptionManagementView(LoginRequiredMixin, View):
     template_name = 'subscription/management.html'
 
     def get(self, request):
 
-        subscriptions = {}
-        for user in UserFollows.objects.filter(user=request.user.id):
-            subscriptions[user.followed_user] = user.id
+        # Get active subscriptons for delete forms and to skip it in new subscription list
+        already_followed = []
+        active_subscriptions = {}
+        for sub in UserFollows.objects.filter(user=request.user.id):
+            active_subscriptions[sub.followed_user] = forms.RemoveSubscriptionForm(initial={'delete_sub': sub.id})
+            already_followed.append(sub.followed_user)
 
-        # sort ???
+        # Choice list -> Form creates in the template
+        choices = {}
+        for user in User.objects.all():
+            if user not in already_followed and user.username != 'admin' and user.username != request.user.username:
+                choices[user.username] = user.id
 
-        return render(request, self.template_name, context={'subscriptions': subscriptions})
+        active_followers = []
+        for sub in UserFollows.objects.filter(followed_user=request.user.id):
+            active_followers.append(sub.user)
 
 
-
+        # Sort needed ?
+        return render(request, self.template_name, context={'choices': choices,
+                                                            'subscriptions': active_subscriptions,
+                                                            'followers': active_followers})
 
     def post(self, request):
 
-        forms_unsubscribe = forms.DeleteSubscriptionForm(request.POST)
+        if 'delete_sub' in request.POST:
+            forms_unsubscribe = forms.RemoveSubscriptionForm(request.POST)
 
-        if forms_unsubscribe.is_valid():
-            return redirect('home')
+            if forms_unsubscribe.is_valid():
 
-        else:
-            print('prOut de mammouth')
-            return render(request, self.template_name, context={'forms_unsubscribe': forms_unsubscribe})
+                UserFollows.objects.get(id=forms_unsubscribe.cleaned_data['delete_sub']).delete()
+                return redirect('subscription_management')
 
+            else:
+                return render(request, self.template_name)
 
-class DeleteSubscriptionView(LoginRequiredMixin, View):
+        elif 'new_subscription' in request.POST:
 
-    def get(self, request, userfollows_id):
-        subscription = UserFollows.objects.get(id=userfollows_id)
-        subscription.delete()
+            print(request.POST.get('new_subscription'))
+            followed_user = User.objects.get(id=request.POST.get('new_subscription'))
 
-        return redirect('subscription_management')
+            new_entry = UserFollows()
+            new_entry.user = request.user
+            new_entry.followed_user = followed_user
+            new_entry.save()
+
+            return redirect('subscription_management')
